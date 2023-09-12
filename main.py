3a13d62874ddd20ece4d20b811dc5b6faef0b856
santi <santiago.penate.vera@gmail.com>
2023-09-12 21:26:40 +0100

    initial work

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import ssl
+
 
 import grid2op
 from newtonBackend import NewtonBackend
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # or any other name.  -> this is sent to the backend `/home/whatehver/grid2op_datasets/l2rpn_case14_sandbox/grid.json`
 env_name = "l2rpn_case14_sandbox"
